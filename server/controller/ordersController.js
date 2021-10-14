cc9e50439b9b7d8468888fcce4483a9da8dfacc2
Lediane <lediane141@gmail.com>
2021-10-14 15:35:41 -0300

    iniciando no heroku

--- server/controller/ordersController.js
+++ server/controller/ordersController.js
@@ -1,23 +1,18 @@
 const getAllOrders = (req, res) => {
-console.log('pegando todas as orders');
   res.send('Request getAllOlders feita');
 };
 const getOrderById = (req, res) => {
-console.log('pegando order por id');
   res.send('Request getOrdersById feita');
 };
 const postOrder = (req, res) => {
-  console.log('postOrder');
   res.send('Request postOrder feita');
 };
 
 const putOrder = (req, res) => {
-  console.log('putOrder');
   res.send('Request putOrder feita');
 };
 
 const deleteOrder = (req, res) => {
-  console.log('deletar order');
   res.send('Request deleteOrder feita');
 };
 
