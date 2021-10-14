265faab2928ad2542d678f108fe0baeffa41cbb2
Lediane <lediane141@gmail.com>
2021-10-14 18:37:28 -0300

    arrumando scripts

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,15 @@
 const { Router } = require('express');
 const ExampleRouter = require('./exampleRouter');
-// const OrderRouter = require('./orderRouter');
-// const ProductsRouter = require('./productsRouter');
-// const UsersRouter = require('./usersRouter');
+const OrderRouter = require('./orderRouter');
+const ProductsRouter = require('./productsRouter');
+const UsersRouter = require('./usersRouter');
 
 const router = Router();
 
 // aqui vai todas as rotas
 router.use('/example', ExampleRouter);
-// router.use('/orders', OrderRouter);
-// router.use('/products', ProductsRouter);
-// router.use('/users', UsersRouter);
+router.use('/orders', OrderRouter);
+router.use('/products', ProductsRouter);
+router.use('/users', UsersRouter);
 
 module.exports = router;
