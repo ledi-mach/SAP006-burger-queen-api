5c949d95325de249c272b1ea6bc8a88e3afa7cfd
Lediane <lediane141@gmail.com>
2021-10-18 11:33:36 -0300

    adicionando tabela products e orders

---
+++ server/db/models/products.js
@@ -0,0 +1,24 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Products extends Model {
+
+  static associate(models) {
+      // define association here
+    }
+  };
+  Products.init({
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    flavor: DataTypes.STRING,
+    complement: DataTypes.STRING,
+    image: DataTypes.STRING,
+    type: DataTypes.STRING,
+    sub_type: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Products',
+  });
+  return Products;
+};
